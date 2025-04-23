80f7203e544574ba2a819bd9e301029260633364
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2025-04-23 19:11:58 +0000

    MAINT: Update of bindings for data storage
    Added bindings for the int specification. Still not working, though

--- bindings/python/compressed_storage_bindings.cpp
+++ bindings/python/compressed_storage_bindings.cpp
@@ -17,4 +17,15 @@ PYBIND11_MODULE(compressed_storage, m)
         .def("rows_in_col", &CompressedDataStorage<double>::rows_in_col)
         .def("values", &CompressedDataStorage<double>::values)
         .def("value", &CompressedDataStorage<double>::value);
+
+    py::class_<CompressedDataStorage<int>>(m, "CompressedDataStorageInt")
+        .def(py::init<int, int>())  // Bind the empty matrix constructor
+        .def(py::init<int, int, const std::vector<int>&, const std::vector<int>&,
+                      const std::vector<int>&>())  // Bind the constructor with data
+        .def("num_rows", &CompressedDataStorage<int>::num_rows)
+        .def("num_cols", &CompressedDataStorage<int>::num_cols)
+        .def("cols_in_row", &CompressedDataStorage<int>::cols_in_row)
+        .def("rows_in_col", &CompressedDataStorage<int>::rows_in_col)
+        .def("values", &CompressedDataStorage<int>::values)
+        .def("value", &CompressedDataStorage<int>::value);
 }
