245b8c224da9674a8ec4409162d405518ff2356c
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2025-04-26 16:12:01 +0000

    MAINT: Update python binding mpxa
    Import now works without errors. Next make it possible to actually use it

--- bindings/python/mpxa_bindings.cpp
+++ bindings/python/mpxa_bindings.cpp
@@ -1,8 +1,8 @@
 #include <pybind11/pybind11.h>
 
-// #include "compressed_storage_bindings.cpp"  // Include the header or source file
-// #include "grid_bindings.cpp"                // Include the header or source file
-#include "tensor_bindings.cpp"  // Include the header or source file
+#include "compressed_storage_bindings.cpp"  // Include the header or source file
+#include "grid_bindings.cpp"                // Include the header or source file
+#include "tensor_bindings.cpp"              // Include the header or source file
 
 namespace py = pybind11;
 
@@ -11,9 +11,9 @@ PYBIND11_MODULE(mpxa, m)
     // m.doc() = "MPXA Python module";  // Optional module docstring
 
     // Initialize bindings for compressed_storage
-    // init_compressed_storage(m);
+    init_compressed_storage(m);
     // Initialize bindings for grid
-    // init_grid(m);
+    init_grid(m);
     // Initialize bindings for tensor
     init_tensor(m);
 
