c4ac1e2efa80e93191692a50e5607dfba4d0cd47
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2025-04-26 14:12:26 +0000

    MAINT: Python binding files are not separate modules
    Instead define init function that can be invoked by the file that defines the python module

---
+++ bindings/python/tensor_bindings.cpp
@@ -0,0 +1,13 @@
+#include <pybind11/pybind11.h>
+
+#include "../include/tensor.h"
+
+namespace py = pybind11;
+
+// PYBIND11_MODULE(tensor, m)
+void init_tensor(py::module_ &m)
+{
+    py::class_<SecondOrderTensor>(m, "SecondOrderTensor")
+        .def(py::init<int, int, std::vector<double>>());
+    // .def("some_method", &SecondOrderTensor::some_method);  // Example method
+}
