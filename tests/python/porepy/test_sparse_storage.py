dd8f2796f864a87438fdf600dba42fc7256d8a9a
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2025-05-06 17:17:59 +0000

    TST: Test of PorePy sparse matrix binding

---
+++ tests/python/porepy/test_sparse_storage.py
@@ -0,0 +1,25 @@
+import porepy as pp
+import numpy as np
+import scipy.sparse as sps
+
+
+import porepy_bridge
+
+
+def test_sparse_matrix_conversion():
+    np.random.seed(0)
+
+    # Create a sparse matrix
+    mat_sps = sps.random(10, 12, density=0.3, format="csr")
+
+    mat_mpxa = porepy_bridge._sparse_matrix_conversion(mat_sps)
+
+    # Check the shape
+    assert mat_mpxa.num_rows() == mat_sps.shape[0]
+    assert mat_mpxa.num_cols() == mat_sps.shape[1]
+
+    # Check the data by looping over all entries. This is not efficient, but will check
+    # both zeros and non-zeros.
+    for i in range(mat_sps.shape[0]):
+        for j in range(mat_sps.shape[1]):
+            assert mat_mpxa.value(i, j) == mat_sps[i, j]
