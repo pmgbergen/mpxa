252fa31c86f96d48718bdc7ac18d38bd731dfe72
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2025-05-23 18:24:29 +0000

    FEAT: Include files for common components of multipoint methods

---
+++ include/multipoint_common.h
@@ -0,0 +1,68 @@
+#ifndef MULTIPOINT_COMMON_H
+#define MULTIPOINT_COMMON_H
+
+#include <Eigen/Dense>
+#include <array>
+#include <vector>
+
+#include "grid.h"
+
+using Eigen::MatrixXd;
+
+// This file contains common definitions and includes for the multipoint flux and
+// stress methods.
+
+class BasisConstructor
+{
+   public:
+    // Constructor
+    BasisConstructor(const int dim);
+
+    // Destructor
+    ~BasisConstructor() = default;
+
+    // Function to compute the basis functions and their gradients. The signature is
+    // highly uncertain.
+    void compute_basis_functions(const std::vector<std::array<double, 3>>& coords,
+                                 std::vector<double>& basis_functions, );
+
+   private:
+    // Dimension of the problem
+    int m_dim;
+    // Matrix to store the coordinates of the nodes. Will be the left-hand side of the
+    // equation.
+    MatrixXd m_coord_matrix;
+    // Matrix to store the computed basis functions.
+    MatrixXd m_basis_matrix;
+    // Matrix to represent the right hand side. Should be an identity matrix.
+    MatrixXd m_rhs_matrix;
+};
+
+class InteractionRegion
+{
+   public:
+    // Constructor
+    InteractionRegion(const int node, const int dim, Grid& grid);
+
+    // Destructor
+    ~InteractionRegion() = default;
+
+   private:
+    // Idea is to use an index counter of the vector to get the local index. This may be
+    // insufficient, in which case we may need a map, double map of sorts or a pair.
+    std::vector<int> m_faces;
+    std::vector<int> m_cells;
+    // For each cell its associated faces. There will be nd of these.
+    std::vector<std::array<int, grid.dim>> m_faces_of_cells;
+    // For each face a single cell used to compute the flux.
+    std::vector<int> m_main_cell_of_faces;
+    // The node around which the interaction region is built.
+    const int m_node;
+    // The dimension of the problem to be discretize (do not confuse with the dimension
+    // of the grid). How will this impact the topology of the interaction region?
+    const int m_dim;
+    // The grid object
+    Grid& m_grid;
+};
+
+#endif  // MULTIPOINT_COMMON_H
