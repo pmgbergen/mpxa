0c6f2054cb4be85addee8e3205f8e4149aae1f69
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2025-06-05 18:26:54 +0000

    BUG: In mpfa storage construction of sparse flux matrix

--- src/mpfa.cpp
+++ src/mpfa.cpp
@@ -310,33 +310,45 @@ ScalarDiscretization mpfa(const Grid& grid, const SecondOrderTensor& tensor,
 
     for (const int index : sorted_indices)
     {
-        // If the row index has changed, we need to update the row_ptr.
+        // If the row index has changed, that is, we have reached a new face, we need to
+        // update the row_ptr.
         if (flux_matrix_row_idx[index] != previous_row)
         {
+            // We need to store the flux values for the row, and empty the map for the next
+            // row.
+            for (const auto& pair : flux_matrix_values_map)
+            {
+                col_idx.push_back(pair.first);
+                flux_values.push_back(pair.second);
+            }
+            flux_matrix_values_map.clear();
             // If we have a new row, we need to update the row_ptr.
-            row_ptr.push_back(flux_matrix_values_map.size());
+            row_ptr.push_back(flux_values.size());
             previous_row = flux_matrix_row_idx[index];
+
+            int counter = 0;
+            for (const double col_index : flux_matrix_col_idx[index])
+            {
+                // Store the flux values in a map to avoid duplicates.
+                flux_matrix_values_map[col_index] += flux_matrix_values[index][counter];
+                ++counter;
+            }
         }
-        for (const double col_index : flux_matrix_col_idx[index])
+        for (const auto& pair : flux_matrix_values_map)
         {
-            // Store the flux values in a map to avoid duplicates.
-            flux_matrix_values_map[col_index] += flux_matrix_values[index][col_index];
+            col_idx.push_back(pair.first);
+            flux_values.push_back(pair.second);
         }
-    }
-    for (const auto& pair : flux_matrix_values_map)
-    {
-        col_idx.push_back(pair.first);
-        flux_values.push_back(pair.second);
-    }
-    // Add the last row pointer.
-    row_ptr.push_back(col_idx.size());
+        // Add the last row pointer.
+        row_ptr.push_back(col_idx.size());
 
-    // Create the compressed data storage for the flux.
-    auto flux_storage = std::make_shared<CompressedDataStorage<double>>(
-        grid.num_faces(), grid.num_cells(), row_ptr, col_idx, flux_values);
-    // Create the scalar discretization object and return it.
-    ScalarDiscretization discretization;
-    discretization.flux = flux_storage;
+        // Create the compressed data storage for the flux.
+        auto flux_storage = std::make_shared<CompressedDataStorage<double>>(
+            grid.num_faces(), grid.num_cells(), row_ptr, col_idx, flux_values);
+        // Create the scalar discretization object and return it.
+        ScalarDiscretization discretization;
+        discretization.flux = flux_storage;
 
-    return discretization;
+        return discretization;
+    }
 }
