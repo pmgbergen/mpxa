2ce4de8d945b7fb755bbb4cdba7a9f5706ee3e33
Jakub Both <jakub.both@uib.no>
2025-12-02 20:22:35 +0000

    add analogue of pp.Tpfa

--- src/mpxa/porepy_ad_bridge.py
+++ src/mpxa/porepy_ad_bridge.py
@@ -1,28 +1,27 @@
 from __future__ import annotations
 
 import porepy as pp
+import mpxa
 
-from . import porepy_bridge  # Import the bridge functions module
 
-
-class MpfaNonAd(pp.Mpfa):
+class Tpfa(pp.Tpfa):
 
     def __init__(self, keyword: str) -> None:
-        super(MpfaNonAd, self).__init__(keyword)
+        super(Tpfa, self).__init__(keyword)
 
     def discretize(self, sd: pp.Grid, data: dict) -> None:
-        
+
         K_pp = data[pp.PARAMETERS][self.keyword]["second_order_tensor"]
         bc_pp = data[pp.PARAMETERS][self.keyword]["bc"]
         g_pp = sd
 
-        K_cpp = porepy_bridge.convert_tensor(K_pp, g_pp.dim)
-        bc_cpp = porepy_bridge.convert_bc(bc_pp)
-        g_cpp = porepy_bridge.convert_grid(g_pp)
+        K_cpp = mpxa.convert_tensor(K_pp, g_pp.dim)
+        bc_cpp = mpxa.convert_bc(bc_pp)
+        g_cpp = mpxa.convert_grid(g_pp)
 
         # Import _mpxa directly to avoid circular import
-        from _mpxa import mpfa
-        mpfa_cpp = mpfa(g_cpp, K_cpp, bc_cpp)
+        #from _mpxa import tpfa
+        tpfa_cpp = mpxa.tpfa(g_cpp, K_cpp, bc_cpp)
 
         # Prepare the cpp discretization
         for attribute in [
@@ -33,4 +32,34 @@ class MpfaNonAd(pp.Mpfa):
             "bound_pressure_cell",
             "bound_pressure_vector_source",
         ]:
-            data[pp.DISCRETIZATION_MATRICES][self.keyword][attribute] = porepy_bridge.convert_matrix(getattr(mpfa_cpp, attribute))
+            data[pp.DISCRETIZATION_MATRICES][self.keyword][attribute] = mpxa.convert_matrix(getattr(tpfa_cpp, attribute))
+
+class Mpfa(pp.Mpfa):
+
+    def __init__(self, keyword: str) -> None:
+        super(Mpfa, self).__init__(keyword)
+
+    def discretize(self, sd: pp.Grid, data: dict) -> None:
+        
+        K_pp = data[pp.PARAMETERS][self.keyword]["second_order_tensor"]
+        bc_pp = data[pp.PARAMETERS][self.keyword]["bc"]
+        g_pp = sd
+
+        K_cpp = mpxa.convert_tensor(K_pp, g_pp.dim)
+        bc_cpp = mpxa.convert_bc(bc_pp)
+        g_cpp = mpxa.convert_grid(g_pp)
+
+        # Import _mpxa directly to avoid circular import
+        #from _mpxa import mpfa
+        mpfa_cpp = mpxa.mpfa(g_cpp, K_cpp, bc_cpp)
+
+        # Prepare the cpp discretization
+        for attribute in [
+            "vector_source",
+            "flux",
+            "bound_flux",
+            "bound_pressure_face",
+            "bound_pressure_cell",
+            "bound_pressure_vector_source",
+        ]:
+            data[pp.DISCRETIZATION_MATRICES][self.keyword][attribute] = mpxa.convert_matrix(getattr(mpfa_cpp, attribute))
